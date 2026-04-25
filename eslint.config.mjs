3c929e371bc2bc91c6c10f5c234b3480d36feaa2
brubru6707 <brubrurodriguez456@gmail.com>
2026-04-25 00:02:09 -0400

    fix: downgraded eslint to older version

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,15 @@
-import nextCoreWebVitals from "eslint-config-next/core-web-vitals.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 /** @type {import("eslint").Linter.Config[]} */
 const eslintConfig = [
-  ...(Array.isArray(nextCoreWebVitals) ? nextCoreWebVitals : [nextCoreWebVitals]),
+  ...compat.extends("next/core-web-vitals"),
   {
     ignores: [".venv/**", "**/.venv/**"],
   },
